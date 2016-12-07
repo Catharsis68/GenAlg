621f9922ab7cc6938425ef6e3b404a94f39107c9
Markus Frindt <markuslawl@gmail.com>
2016-12-07 11:24:39 +0100

    added main

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by mfrindt on 02.11.16.
+ */
+public class Main {
+
+
+
+    // Ziel wird an den Algorithmus übergeben zb Genom mit dem x
+
+
+    public static void main(String[] args) {
+
+        GenAlg algorithmus = new GenAlg();
+
+    }
+
+
+}
